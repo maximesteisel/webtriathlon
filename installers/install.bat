f35384d57576372db5b998cfd22498666fd509bb
Maxime Steisel <maximesteisel@gmail.com>
2018-08-25 11:02:51 +0200

    fix install.bat

--- installers/install.bat
+++ installers/install.bat
@@ -1,5 +1,7 @@
+
+exe\01python-2.7.12.msi
 exe\pygtk-all-in-one-2.24.2.win32-py2.7.msi
 exe\pywin32-219.win32-py2.7.exe
-exe\python-2.7.12.msi
-py -2.7 -m ensurepip --upgrade
-py -2.7 -m pip install -r requirements.txt
+
+C:\Python27\python.exe -m ensurepip --upgrade
+C:\Python27\python.exe -m pip install -r requirements.txt
--- release.sh
+++ release.sh
@@ -3,6 +3,6 @@ cd build
 ln -s ../installers
 ln -s ../README
 echo "creating release tarball"
-zip -r webtriathlon-r`git rev-list --count HEAD`-db`python server.py dbversion`.zip README.*  server.py encoder.pyw installers
+zip -r webtriathlon-r`git rev-list --count HEAD`-db`python server.py dbversion`.zip README.*  server.py encoder.py installers
 echo "cleaning up"
 rm installers README
